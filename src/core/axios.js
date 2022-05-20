4f51e6363ca6a3afb40787b143372af7009fa8cc
Dennis <1044442@gmail.com>
2022-05-20 20:28:26 +0300

    cors fixed

--- src/core/axios.js
+++ src/core/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://chat-server-dennis.herokuapp.com/';
 axios.defaults.headers.common['token'] = window.localStorage.token;
 // axios.defaults.headers.common['my-custom-header'] = 'abcd';
-axios.defaults.headers.common['withCredentials'] = true;
+axios.defaults.headers.common['credentials'] = true;
 
 window.axios = axios;
 
