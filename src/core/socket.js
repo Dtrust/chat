edc47ef19e9fd7ec830cb2c1fc54e715c385482e
Dennis <1044442@gmail.com>
2022-05-20 22:20:19 +0300

    cors fixed

--- src/core/socket.js
+++ src/core/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 const socket = io('https://chat-server-dennis.herokuapp.com/',
 	{
-		withCredentials: true,
+		// withCredentials: true,
 		extraHeaders: {
 			"my-custom-header": "abcd"
 		}
