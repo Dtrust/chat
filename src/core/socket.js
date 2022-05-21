94ca378830e244d66cfc2a145f71c7e434880d12
Dennis <1044442@gmail.com>
2022-05-21 22:11:19 +0300

    cors debug

--- src/core/socket.js
+++ src/core/socket.js
@@ -4,9 +4,9 @@ import { io } from "socket.io-client";
 const socket = io('https://chat-server-dennis.herokuapp.com/',
 	{
 		// withCredentials: true,
-		extraHeaders: {
-			"sky-messenger-header": "request"
-		}
+		// extraHeaders: {
+		// 	"sky-messenger-header": "request"
+		// }
 	});
 
 // const socket = io(window.location.origin.replace('3000', '3003'), {
