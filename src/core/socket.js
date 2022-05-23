bf3bd8bf4b2aed40e363cd97f1cfd4b65c23bcd6
Dennis Dovziy <1044442@gmail.com>
2022-05-23 22:38:44 +0300

    cors trouble

--- src/core/socket.js
+++ src/core/socket.js
@@ -11,9 +11,9 @@ const socket = io('https://chat-server-dennis.herokuapp.com/',
 
 // const socket = io(window.location.origin.replace('3000', '3003'), {
 // 	// 		// withCredentials: true,
-// 	extraHeaders: {
-// 		"sky-messenger-header": "request"
-// 	}
+// 	// extraHeaders: {
+// 	// 	"sky-messenger-header": "request"
+// 	// }
 // });
 
 
