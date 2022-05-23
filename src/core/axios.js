c8babff69b90dac0f2edff894023052f43a4469b
Dennis Dovziy <1044442@gmail.com>
2022-05-23 20:54:16 +0300

    cors trouble

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'https://chat-server-dennis.herokuapp.com/';
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://chat-server-dennis.herokuapp.com/';
 // axios.defaults.baseURL = 'http://localhost:3003';
 axios.defaults.headers.common['token'] = window.localStorage.token;
 // axios.defaults.headers['sky-messenger-header'] = 'request';
